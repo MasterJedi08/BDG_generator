00a89b773459d69826b35277a1e18be38d6e92ef
Ashley <ashley.jablonski@students.loy.org>
2020-06-25 18:28:48 -0700

    8:26PM CST update
    finally got stuff working!! now to fix the text generation...
    (P.S. I am not uploading the debugging logs)

--- bdg.py
+++ bdg.py
@@ -7,12 +7,13 @@
 
 # IMPORTS
 from pathlib import Path
-import nltk
+import nltk, random
 from stop import stop_words
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename = "log_bdg14.txt", level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename = "log_bdg14.txt", level=logging.ERROR, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.DEBUG)
 logging.debug("Start of program")
  
 # TODO: get transcripts + transcripts into lists
@@ -41,29 +42,59 @@ all_transcripts = [bowser, castlevania, dark_souls, e3, fallout, fire_emblem, ga
     kingdom_hearts, kirby, mario_retire, megaman, osha_violations, pet_hp, sims, skyrim_book_report, sonic,
     stamina, waluigi, zelda]
 
-all_bigrams_list = []
-# IDEA: use for loop to go thru each one
-for current_file in all_transcripts:
-    logging.DEBUG("file running: %s" % (current_file[:20]))
+transcripts_list = []
+transcript_word_count = 0
+
+for current_file in all_transcripts:    
     # list comprehension: joins words in file
-    all_word_list = '\n'.join([item for item in current_file]))
-
-    # TODO: remove stop-words from lists
-    removed_list = [word for word in all_word_list if word not in stop_words]
-    logging.INFO("file after stop_wordss removed: ")
+    word_list = (' '.join([item for item in current_file]))
+    word_list = word_list.split(' ')
+    transcript_word_count += (len(word_list))
+    for i in range(len(word_list)):
+        transcripts_list.append(word_list[i])
     
-    # TODO: use bigram? trigram? model << try bi first then tri
-    file_bigram = nltk.bigrams(removed_list)
-    all_bigrams_list.append(file_bigram)
+logging.debug("completed adding to all transcripts list: %s" %(transcripts_list))
+logging.debug("completed adding transcript length to list: %s" %(transcript_word_count))
 
-master_cfd = nltk.ConditionalFreqDist(all_bigrams_list[1])
-print(master_cfd)
+# word for word in transcripts_list if word not in stop_words
+transcripts_removed_list = []
+for word in transcripts_list:
+    if word in stop_words:
+        pass
+    else:
+        transcripts_removed_list.append(word)
+
+logging.debug("file after stop_words removed: %s" %(transcripts_removed_list))
+
+# TODO: use bigram? trigram? model << try bi first then tri
+transcripts_bigram = nltk.bigrams(transcripts_removed_list)
+logging.debug("file bigram: %s" %(transcripts_bigram))
+
+master_cfd = nltk.ConditionalFreqDist(transcripts_bigram)
+logging.debug("cfd created %s" %(master_cfd))
+
+#----COPIED FROM SETHS NLP----WILL CHANGE LATER
+def generate_text(cfd, n, word='', sz=10):
+  # if no starting word specified, use a random word in the conditional freq distrib
+  if not word or not cfd[word]:
+    # get random key
+    r = random.randint(0, len(cfd.keys())-1)
+    word = list(cfd.keys())[r]
+  
+  # loop through number of desired words, and use previous word as next key
+  for i in range(n):
+    # print current word
+    yield word
+    # index to get most common "sz" words and randomly select one as next word
+    word = random.choice(cfd[word].most_common()[:sz])[0]
+
+# 2670 = calculated avg word count of transcript
+print(' '.join([item for item in generate_text(master_cfd, 2670)]))
 
 # TODO: function for generating text - params: cfd, length of generated text, 
 #           starting word, sz << sz is how many words from cfd are grabbed
 def script_generator(cfd, script_length, start_word='', size=10):
     # TODO: if no starting word, get random one
-    
     pass
     # TODO: loop thru num of desired words and use prev word as next key
 
